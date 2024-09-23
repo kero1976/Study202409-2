b31ea7ab047c739f4b29412807c563b959021c67
kero <kero1976@outlook.jp>
2024-09-23 10:10:36 +0900

    add

--- code/python/work/awssample/awssample/connect/connect.py
+++ code/python/work/awssample/awssample/connect/connect.py
@@ -17,7 +17,11 @@ class Connect():
     create boto3.resource and boto3.client
     """
 
-    def __init__(self, service: str, profile_name: str = None, region_name: str = None):
+    def __init__(self,
+                 service: str,
+                 profile_name: str = None,
+                 region_name: str = None,
+                 endpoint_url: str = None):
         """コンストラクタ
 
         Args:
@@ -26,6 +30,7 @@ class Connect():
         self.service = service
         self.profile_name = profile_name
         self.region_name = region_name
+        self.endpoint_url = endpoint_url
 
     def get_resource(self):
         """resourceの取得
@@ -34,8 +39,8 @@ class Connect():
             _type_: resource
         """
         if self.profile_name:
-            return self._get_session().resource(self.service)
-        return boto3.resource(self.service)
+            return self._get_session().resource(self.service, endpoint_url=self.endpoint_url)
+        return boto3.resource(self.service, endpoint_url=self.endpoint_url)
 
     def get_client(self):
         """clientの取得
@@ -44,8 +49,8 @@ class Connect():
             _type_: client
         """
         if self.profile_name:
-            return self._get_session().client(self.service)
-        return boto3.client(self.service)
+            return self._get_session().client(self.service, endpoint_url=self.endpoint_url)
+        return boto3.client(self.service, endpoint_url=self.endpoint_url)
 
     def _get_session(self):
         """profile_nameを使用してセッションを作成する
--- code/python/work/awssample/tests/test_main.py
+++ code/python/work/awssample/tests/test_main.py
@@ -10,13 +10,12 @@ from moto import mock_aws
 TABLE_NAME = "test20240922"
 
 
-@mock_aws
 def test_case1():
     """テーブルを作成し、テーブルを削除する
     """
-    dynamodb = Connect("dynamodb", "shijo", "ap-northeast-1")
+    dynamodb = Connect("dynamodb", endpoint_url="http://localhost:8000")
     insert = Insert(dynamodb)
     Create(dynamodb).create_table(TABLE_NAME)
-    for i in range(1):
-        insert.add_item(TABLE_NAME, {"id": f"test4{i}", "foo": "bar"})
-    Delete(dynamodb).delete_table(TABLE_NAME)
+    for i in range(10000):
+        insert.add_item(TABLE_NAME, {"id": f"test4{i}", "foo1": "1"})
+    # Delete(dynamodb).delete_table(TABLE_NAME)
