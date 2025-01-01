98bf5fba4fe55d5fb64eae24907499359a064ba4
kero1976 <kero1976@outlook.jp>
2025-01-01 13:00:44 +0900

    .

--- code/docker/java/App.java
+++ code/docker/java/App.java
@@ -1,5 +1,32 @@
+import java.util.Properties;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Docker!");
+
+        System.out.println("****************************************************************************************");
+        System.out.println("Properties");
+        System.out.println("****************************************************************************************");
+        
+        Properties properties = System.getProperties();
+
+        // すべてのプロパティと値を表示
+        properties.forEach((key, value) -> {
+            System.out.println(key + ": " + value);
+        });
+
+        System.out.println("");
+        System.out.println("****************************************************************************************");
+        System.out.println("env");
+        System.out.println("****************************************************************************************");
+
+        // 環境変数を取得
+        Map<String, String> env = System.getenv();
+
+        // すべての環境変数と値を表示
+        env.forEach((key, value) -> {
+            System.out.println(key + ": " + value);
+        });
     }
 }
