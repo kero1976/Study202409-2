1eed97ff57281abf917435c5715db50825836294
kero <kero1976@outlook.jp>
2025-04-19 01:29:39 +0900

    .

--- code/python/work/csv-tool/tests/data/read_default_csv/test_read_default_csv.py
+++ code/python/work/csv-tool/tests/data/read_default_csv/test_read_default_csv.py
@@ -2,7 +2,26 @@ from csv_tool.read_default_csv import read_csv_file
 import pytest
 
 
-def test1():
-    assert 1 == 1
+def test_data1():
+    """CSVファイルの読み込みテスト1
+    列数は1行数は5のCSVファイルを読み込む。
+    """
     result = read_csv_file("./tests/data/read_default_csv/data1.txt")
     assert len(result) == 5
+    assert result[0] == [""]
+    assert result[1] == [""]
+    assert result[2] == ["不明"]
+    assert result[3] == [""]
+    assert result[4] == ["未設定"]
+
+def test_data2():
+    """CSVファイルの読み込みテスト2
+    列数は2行数は5のCSVファイルを読み込む。
+    """
+    result = read_csv_file("./tests/data/read_default_csv/data2.txt")
+    assert len(result) == 5
+    assert result[0] == ["ID",""]
+    assert result[1] == ["名前",""]
+    assert result[2] == ["住所","不明"]
+    assert result[3] == ["性別",""]
+    assert result[4] == ["オプション情報","未設定"]
