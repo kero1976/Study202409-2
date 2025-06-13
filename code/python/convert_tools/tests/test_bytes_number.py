c85f6ab6bd0e95b7cb8e64d43ff8cf982942d5cc
kero <kero1976@outlook.jp>
2025-06-13 18:59:15 +0900

    .

--- code/python/convert_tools/tests/test_bytes_number.py
+++ code/python/convert_tools/tests/test_bytes_number.py
@@ -25,8 +25,26 @@ def test_bytes_to_int_little_endian_signed():
     result = bytes_to_int(byte_array, byteorder='little', signed=True)
     assert result == -1
 
+
 def test_bytes_to_int_empty_array():
     """空のバイト配列のテスト"""
     byte_array = b''
     result = bytes_to_int(byte_array, byteorder='big', signed=False)
     assert result == 0
+
+
+def test_255_1():
+    """0xFFのテスト1"""
+    result = bytes_to_int(b'\xFF')
+    assert result == 255
+
+
+def test_255_2():
+    """0xFFのテスト2"""
+    result = bytes_to_int(b'\x00\xFF')
+    assert result == 255
+
+def test_255_3():
+    """0xFFのテスト3"""
+    result = bytes_to_int(b'\x00\x00\xFF')
+    assert result == 255
